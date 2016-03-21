4ffd95f0c0c5f3a6818a367d13d01be39b37d491
Terry Christopher Martin <tcmartin24@yahoo.com>
2016-03-21 00:41:23 -0500

    Added asynchronous mode with logging

--- src/main/java/com/example/BobDemoApplication.java
+++ src/main/java/com/example/BobDemoApplication.java
@@ -3,6 +3,8 @@ package com.example;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -10,7 +12,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @RestController
-
+@EnableAsync
 public class BobDemoApplication {
 
 	public static void main(String[] args) {
@@ -29,6 +31,12 @@ public class BobDemoApplication {
 		result.put("greeting", "The ABCs of 1-2-3s baby!");
 		result.put("pi", PiDigits.pi_digits(digits > 0 ? digits : 100));
 		return result;
+	}
+
+	@RequestMapping(value = "/piAsync/{digits}", method = RequestMethod.GET)
+	@Async
+	public Map<String, String> calculatePiAsync(@PathVariable int digits) {
+		return this.calculatePi(digits);
 	}
 
 	@RequestMapping(value = "/cfData", method = RequestMethod.GET)
--- src/main/java/com/example/PiDigits.java
+++ src/main/java/com/example/PiDigits.java
@@ -1,10 +1,14 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class PiDigits {
 	private static final int SCALE = 10000;
 	private static final int ARRINIT = 2000;
 
 	public static String pi_digits(int digits){
+		short logIn = 20;
 		StringBuffer pi = new StringBuffer();
 		int[] arr = new int[digits + 1];
 		int carry = 0;
@@ -13,6 +17,11 @@ public class PiDigits {
 			arr[i] = ARRINIT;
 
 		for (int i = digits; i > 0; i-= 14) {
+			if (logIn-- == 0) {
+				logIn = 20;
+				log.info("We're at iteration " + i);
+			}
+
 			int sum = 0;
 			for (int j = i; j > 0; --j) {
 				sum = sum * j + SCALE * arr[j];
